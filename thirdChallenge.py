f35feb628631b4af548923fc14e8bb5b406896b5
Mary Abuela <121141682+haru0702@users.noreply.github.com>
2024-02-29 16:37:48 +0800

    update format

--- thirdChallenge.py
+++ thirdChallenge.py
@@ -9,18 +9,18 @@ def b():
 
 # Is it ten? --> Write a function called is_ten that takes one parameter called x. The function must return True if x is 10 and False otherwise.
 def is_ten(x):
-      if x == 10:
+    if x == 10:
             return True
-      else:
+    else:
             return False
-      
+
 # Triple and --> Write a function named triple_and that takes three booleans, combines them with and, and returns the result.
 def triple_and(x, y, z):
     return x and y and z
 
 # Inequality test --> Write a function named different that takes two values and tests whether they are different. Return the result
 def different(x, y):
-      return x != y
+    return x != y
 
 # Casts -> Write a function named x that takes a string as its only parameter. The function should cast its input parameter to an integer and add 3 to it. Don't forget to return the result. For example, calling x("0") should return 3 and calling x("123") should return 126.
 def x(num):
@@ -32,7 +32,7 @@ def test_or_long(x):
 
 # Day of the month --> Write a function named make_day_string that takes an integer named day and returns the string "it is day X of the month" where X is the value of the day parameter. For example, calling make_day_string(3) should return "it is day 3 of the month". Remember that to concatenate a string with an integer, you must cast the integer to a string. Note that the function should return a value. It should not print anything.
 def make_day_string(x):
-     return "it is day " + str(x) + " of the month"
+    return "it is day " + str(x) + " of the month"
 #      print("it is day " + str(x) + " of the month")
 # make_day_string(3)
 
